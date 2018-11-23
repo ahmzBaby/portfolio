7a37bd79e037673b08dc637c2bd70ab1340b143d
ahmzBaby <ahmuproductions@gmail.com>
2018-11-23 14:32:21 -0800

    added static files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello  kids! Eat a human big toe!!!!!!!!!!'))
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/test', (req, res) => res.send('test route'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
